86c9247200c47e3407898d82d46264acc849ba7e
SeyedMohammadMahdi <st.mohammad1380@gmail.com>
2022-12-27 14:22:55 +0330

    added tenFirstLine.h

---
+++ tenFirstLine.h
@@ -0,0 +1,32 @@
+#ifndef TENFIRSTLINE_H
+#define TENFIRSTLINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_FILE_NAME 100
+
+// part g
+int tenFirstLine(FILE* ptr, char filename[MAX_FILE_NAME]){
+	int count = 0;
+	ptr = fopen(filename, "a+");
+	if (NULL == ptr) {
+		printf("file can't be opened \n");
+		return 0;
+	}
+	
+	char str[50];
+	printf("content of this file are: \n");
+	char * pch;
+	while (fgets(str, 50, ptr) != NULL && count<10) {
+		if(strcmp ("EOF", str) != 0){
+			printf("%s", str);
+			count = count + 1;
+		}
+	}
+	printf("\n");
+	fclose(ptr);
+	return 0;
+}
+
+#endif
