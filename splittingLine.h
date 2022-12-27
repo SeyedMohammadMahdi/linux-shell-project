bf9f1b593501585ad51087416ecf9acaac8fea18
SeyedMohammadMahdi <st.mohammad1380@gmail.com>
2022-12-27 14:22:20 +0330

    added splittingLine.h

---
+++ splittingLine.h
@@ -0,0 +1,34 @@
+#ifndef SPLITTINGLINE_H
+#define SPLITTINGLINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_FILE_NAME 100
+
+// part a
+int splittingLine(FILE* ptr, char filename[MAX_FILE_NAME]){
+	
+	ptr = fopen(filename, "a+");
+	if (NULL == ptr) {
+		printf("file can't be opened \n");
+		return 0;
+	}
+	
+	
+	char str[50];
+	printf("content of this file are: \n");
+	char * pch;
+	while (fgets(str, 50, ptr) != NULL) {
+		pch = strtok (str," \n");
+		if(strcmp ("EOF", pch) != 0){
+			printf("%s\n", pch);
+		}
+	}
+	printf("\n");
+	fclose(ptr);
+	return 0;
+}
+
+
+#endif
