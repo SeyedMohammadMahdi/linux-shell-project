c145cd1800eac74f28dc6cc8f13f8e2ca9980104
SeyedMohammadMahdi <st.mohammad1380@gmail.com>
2022-12-27 15:39:53 +0330

    added readme file

---
+++ README.d
@@ -0,0 +1,7 @@
+custom commands
+mainl => mainLines (no comment)
+mast => most frequent word
+remove => remove white spaces
+split => shows the first word of each line
+ten => shows ten first lines
+count => counts the number of lines in a text file
